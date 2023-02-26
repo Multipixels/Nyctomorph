0506bad09232eb7afcde3c17c9fad5010979c707
NathanHartHDCH <n.bernard.hart@gmail.com>
2023-02-25 22:19:20 -0500

    Added static panel (not implemented)

---
+++ Scripts/StaticOverlay.gd
@@ -0,0 +1,19 @@
+extends Sprite
+
+export(int) var intensity := 0;
+
+
+func _on_Timer_timeout() -> void:
+	
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var frame_offset = Vector2(rng.randi_range(0, 48), rng.randi_range(0, 84))
+	
+	if intensity >= 0 and intensity <= 4:
+		frame_offset += Vector2(96*intensity, 0)
+	
+	region_rect = Rect2(frame_offset.x, frame_offset.y, 48, 84)
+
+
+func set_intensity(new_int: int) -> void:
+	intensity = clamp(new_int, 0, 4)
