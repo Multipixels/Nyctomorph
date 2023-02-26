8ff733d89690f8e183d73f6d232d1cabd852ce59
NathanHartHDCH <n.bernard.hart@gmail.com>
2023-02-26 18:55:38 -0500

    Moved mouse override to global singleton

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -4,3 +4,4 @@ var current_shader = null
 
 func _ready() -> void:
 	Engine.time_scale = 1
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
--- Scripts/LogoSplash.gd
+++ Scripts/LogoSplash.gd
@@ -1,8 +1,5 @@
 extends Control
 
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-
 func to_main_menu():
 	get_tree().change_scene("res://Scenes/MainMenu.tscn")
 	
